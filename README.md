007ce08b9f9d6dc0fd737c41ae077cf8e5ea0437
jochen@homeland <no-go@localhost>
2019-03-05 01:14:28 +0100

    better icons, nicer colors, emoji keyboard

--- README.md
+++ README.md
@@ -6,3 +6,55 @@ a black console with green text sms read and send app. All sms in a single text 
 
 [APK file](https://raw.githubusercontent.com/no-go/FatChat/master/app/release/de.digisocken.fatchat.apk)
 
+## License
+
+```
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+```
+
+## Acknowledgements Emoji lib
+
+Emoji Support Lib uses emoji graphics from [emoji-cheat-sheet.com](https://github.com/arvida/emoji-cheat-sheet.com/tree/master/public/graphics/emojis).
+
+This project has a dependency on [Guava](https://github.com/google/guava).
+
+## License Emoji lib
+
+```
+Copyright 2016 Simon Barth
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
--- app/src/main/java/de/digisocken/fatchat/SendActivity.java
+++ app/src/main/java/de/digisocken/fatchat/SendActivity.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
@@ -14,9 +16,19 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.github.data5tream.emojilib.EmojiEditText;
+import com.github.data5tream.emojilib.EmojiGridView;
+import com.github.data5tream.emojilib.EmojiPopup;
+import com.github.data5tream.emojilib.emoji.Emojicon;
 
 import java.util.ArrayList;
 
@@ -27,7 +39,8 @@ public class SendActivity extends AppCompatActivity {
     private ArrayList<PendingIntent> deliveredPIs;
 
     private static SharedPreferences mPreferences;
-    private EditText addredit,msgedit;
+    private EditText addredit;
+    private EmojiEditText emojiEditText;
 
 
     @Override
@@ -41,9 +54,9 @@ public class SendActivity extends AppCompatActivity {
             if(ab != null) {
                 ab.setDisplayShowHomeEnabled(true);
                 ab.setHomeButtonEnabled(true);
-                ab.setDisplayUseLogoEnabled(true);
-                ab.setLogo(R.mipmap.ic_launcher);
-                ab.setTitle(" Send");
+                //ab.setDisplayUseLogoEnabled(true);
+                //ab.setLogo(R.mipmap.ic_launcher);
+                ab.setTitle("Send");
                 ab.setElevation(10);
             }
         } catch (Exception e) {
@@ -51,8 +64,67 @@ public class SendActivity extends AppCompatActivity {
         }
 
         addredit = (EditText) findViewById(R.id.editAddr);
-        msgedit  = (EditText) findViewById(R.id.editMsg);
+        emojiEditText = (EmojiEditText) findViewById(R.id.editMsg);
         addredit.setText(mPreferences.getString("phone", "").trim());
+        final View rootView = findViewById(R.id.rootView);
+        final ImageButton ebtn = (ImageButton) findViewById(R.id.emojiBtn);
+
+        final EmojiPopup popup = new EmojiPopup(rootView, this, getResources().getColor(R.color.colorAccent));
+        popup.setWidth(rootView.getMeasuredWidth());
+        popup.setHeight(rootView.getMeasuredHeight() / 2);
+
+        popup.setOnSoftKeyboardOpenCloseListener(new EmojiPopup.OnSoftKeyboardOpenCloseListener() {
+
+            @Override
+            public void onKeyboardOpen(int keyBoardHeight) { }
+
+            @Override
+            public void onKeyboardClose() {
+                if (popup.isShowing()) popup.dismiss();
+            }
+        });
+
+        popup.setOnEmojiconClickedListener(new EmojiGridView.OnEmojiconClickedListener() {
+
+            @Override
+            public void onEmojiconClicked(Emojicon emojicon) {
+                if (emojicon == null) return;
+                emojiEditText.append(emojicon.getEmoji());
+                popup.dismiss();
+            }
+        });
+
+        popup.setOnEmojiconBackspaceClickedListener(new EmojiPopup.OnEmojiconBackspaceClickedListener() {
+
+            @Override
+            public void onEmojiconBackspaceClicked(View v) {
+                KeyEvent event = new KeyEvent(0, 0, 0, KeyEvent.KEYCODE_DEL, 0, 0, 0, 0, KeyEvent.KEYCODE_ENDCALL);
+                emojiEditText.dispatchKeyEvent(event);
+            }
+        });
+
+        ebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (popup.isShowing()) {
+                    popup.dismiss();
+                } else {
+                    Rect rectgle= new Rect();
+                    Window window= getWindow();
+                    window.getDecorView().getWindowVisibleDisplayFrame(rectgle);
+                    int statusBarHeight = rectgle.top;
+                    Point p = new Point();
+                    ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getSize(p);
+                    Rect windowRect = new Rect();
+                    rootView.getWindowVisibleDisplayFrame(windowRect);
+                    int height = windowRect.height();
+                    int width = windowRect.width();
+                    popup.setHeight((p.y - statusBarHeight) - height);
+                    popup.setWidth(width);
+                    popup.showAtBottom();
+                }
+            }
+        });
     }
 
     public void cancelIt(View view) {
@@ -93,7 +165,7 @@ public class SendActivity extends AppCompatActivity {
             }
         }, new IntentFilter(DELIVERED));
 
-        parts = smsManager.divideMessage(msgedit.getText().toString());
+        parts = smsManager.divideMessage(emojiEditText.getText().toString());
         smsManager.sendMultipartTextMessage(phone, null, parts, sentPIs, deliveredPIs);
     }
 
